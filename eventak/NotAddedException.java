e1a317bc41e5ac5e096c4feea4919d8c53d7b080
Mariam Maarouf <mariam.mrf@hotmail.com>
2016-04-09 03:16:59 +0200

    initial

--- eventak/NotAddedException.java
+++
@@ -1,9 +0,0 @@
-package eventak;
-
-public class NotAddedException extends Exception{
-	private static final long serialVersionUID = -3152816804175318656L; // to fix warning
-
-	NotAddedException(String message){
-		super(message);
-	}
-}
